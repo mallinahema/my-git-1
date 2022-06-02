bbe839bc51842b0082f1f1b56c5d3fd30d3f5fe9
hema <hemamallina34@gmail.com>
2022-06-02 11:50:12 +0530

    first branch 'iss3' commit

--- src/com/cts/Main.java
+++ src/com/cts/Main.java
@@ -1,9 +1,14 @@
 package com.cts;
+import com.cts.service.SomeService;
 
 public class Main {
 	public static void main(String args[])
 	{
 	System.out.println("welcome to the session");
 	System.out.println("hi all ");
+	
+	SomeService ss=new SomeService();
+	System.out.println(ss.findLength("program"));
+	
 }
 }
